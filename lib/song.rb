64926ae948c2fb906a2e2e32b62c67a39eb05fbf
Jess F <robotspacefish@gmail.com>
2019-11-14 01:45:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,17 @@ class Song
     self.class.all << self
   end
 
-  def self.create(song)
-    self.all << song
-    song
+  def self.create(name, artist_name)
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    @@all << song
   end
 
 end
+
+
+s = Song.new
+s.name = "The Ship Song"
+s.artist_name = "Nick Cave"
+s.save
