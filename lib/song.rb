6c1977d6fd2755bc254e66d918a351577ebbac29
Jess F <robotspacefish@gmail.com>
2019-11-14 02:16:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,12 +10,15 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name, artist_name)
+  # def self.create(name, artist_name)
+  #   song = self.new
+  #   song.name = name
+  #   song.artist_name = artist_name
+  #   song.save
+  #   song
+  # end
+  def self.create
     song = self.new
-    song.name = name
-    song.artist_name = artist_name
-    song.save
-    song
   end
 
   def self.new_by_name(name)
